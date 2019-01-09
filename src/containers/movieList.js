1b58072fb0adb1ae70778f9fb81ed12c3c6d4d81
Simon Smulders <simsmulders@gmail.com>
2019-01-09 23:31:45 +0100

    list function added

--- src/containers/movieList.js
+++ src/containers/movieList.js
@@ -5,7 +5,17 @@ import { connect } from 'react-redux';
 class MovieList extends Component {
   renderMovie(movieData) {
     const title = movieData.Title
-    
+    const actors = movieData.Actors
+    const splitActors = actors.split(',')
+    const writers = movieData.Writer
+    const splitWriters = writers.split(',')
+    const genre = movieData.Genre
+    const splitGenre = genre.split(',')
+    const directors = movieData.Director
+    const splitDirectors = directors.split(',')
+    const languages = movieData.Language
+    const splitLanguage = languages.split(',')
+
     if (movieData.Response === 'False') {
       window.alert("Title not recognised")
     } else {
@@ -16,12 +26,12 @@ class MovieList extends Component {
             <p>Rated: {movieData.Rated}</p>
             <p>Released: {movieData.Released}</p>
             <p>Runtime: {movieData.Runtime}</p>
-            <p>Genre: {movieData.Genre}</p>
-            <p>Director: {movieData.Director}</p>
-            <p>Writer: {movieData.Writer}</p>
-            <p>Actors: {movieData.Actors}</p>
-            <p>Plot: {movieData.Plot}</p>
-            <p>Language: {movieData.Language}</p>
+            <p>Genre: {splitGenre.length <= 1 ? `${genre}` : splitGenre.map(gen => <li key={gen}>{gen}</li>)}</p>
+            <p>Director: {splitDirectors.length <= 1 ? `${directors}` : splitDirectors.map(director => <li key={director}>{director}</li>)}</p>
+            <p>Writer: {splitWriters.length <= 1 ? `${writers}` : splitWriters.map(writer => <li key={writer}>{writer}</li>)}</p>
+            <p>Actors: {splitActors.length <= 1 ? `${actors}` : splitActors.map(actor => <li key={actor}>{actor}</li>)}</p>
+            <p>Plot: <i>{movieData.Plot}</i></p>
+            <p>Language: {splitLanguage.length <= 1 ? `${languages}` : splitLanguage.map(language => <li key={language}>{language}</li>)}</p>
             <p>Awards: {movieData.Awards}</p>
             <p><img src={movieData.Poster} alt={movieData.Poster}/></p>
             <div className="ratings">List of ratings: {movieData.Ratings.map(rating => <li className="list-group-item" key={rating.Value}>{rating.Source} : {rating.Value}</li>)}</div>
